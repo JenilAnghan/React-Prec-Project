11857d0f94ab6f40993f547dbda0fd18e06c7be4
jenil <jenilanghan508@gmail.com>
2023-12-07 13:55:13 +0530

    react

--- src/Components/Input/Multipleinput.jsx
+++ src/Components/Input/Multipleinput.jsx
@@ -1,66 +1,90 @@
-import React from "react";
-import { Button, Form, FormGroup, FormText, Input, Label } from "reactstrap";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import {
+  Button,
+  Form,
+  FormGroup,
+  FormText,
+  Input,
+  Label,
+  Table,
+} from "reactstrap";
 
 export default function Multipleinput() {
-  return (
-    <div>
-      <Form>
-        <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input
-            id="exampleEmail"
-            name="email"
-            placeholder="with a placeholder"
-            type="email"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            id="examplePassword"
-            name="password"
-            placeholder="password placeholder"
-            type="password"
-          />
-        </FormGroup>
+  let [user, setuser] = useState({
+    email: "",
+    password: "",
+  });
 
-        <FormGroup>
-          <Label for="exampleText">Text Area</Label>
-          <Input id="exampleText" name="text" type="textarea" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFile">File</Label>
-          <Input id="exampleFile" name="file" type="file" />
-          <FormText>
-            This is some placeholder block-level help text for the above input.
-            It‘s a bit lighter and easily wraps to a new line.
-          </FormText>
-        </FormGroup>
-        <FormGroup tag="fieldset">
-          <legend>Radio Buttons</legend>
-          <FormGroup check>
-            <Input name="radio1" type="radio" />{" "}
-            <Label check>
-              Option one is this and that—be sure to include why it‘s great
-            </Label>
+  let [alluser, setalluser] = useState([]);
+  // {
+  //   email:"test@gmail.com"
+  //   password:"123456789"
+  // }
+
+  const adddata = (e) => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      e.preventDefault();
+      setalluser([...alluser, user]);
+      toast.success("User Added Successfully");
+    } else {
+      toast.error("Please Fill Data");
+    }
+  };
+  return (
+    <>
+      <div>
+        {/* <h1>{user.email}</h1> */}
+        {/* <h1>{user.password}</h1>   */}
+        <Form className="border border-dark p-3 rounded-3">
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+              id="exampleEmail"
+              name="email"
+              placeholder="Enter Your Email"
+              type="email"
+              onChange={(e) => setuser({ ...user, email: e?.target?.value })}
+            />
           </FormGroup>
-          <FormGroup check>
-            <Input name="radio1" type="radio" />{" "}
-            <Label check>
-              Option two can be something else and selecting it will deselect
-              option one
-            </Label>
+
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              id="examplePassword"
+              name="password"
+              placeholder="Enter Your Password"
+              type="password"
+              onChange={(e) => setuser({ ...user, password: e?.target?.value })}
+            />
           </FormGroup>
-          <FormGroup check disabled>
-            <Input disabled name="radio1" type="radio" />{" "}
-            <Label check>Option three is disabled</Label>
-          </FormGroup>
-        </FormGroup>
-        <FormGroup check>
-          <Input type="checkbox" /> <Label check>Check me out</Label>
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
-    </div>
+
+          <Button className="w-100" color="danger" onClick={(e) => adddata(e)}>
+            Submit
+          </Button>
+        </Form>
+
+        <Table striped>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>email</th>
+              <th>passwpred</th>
+            </tr>
+          </thead>
+          <tbody>
+            {alluser.map((e, i) => {
+              return (
+                <tr key={i}>
+                  <th scope="row">{i + 1}</th>
+                  <td>{e.email}</td>
+                  <td>{e.password}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    </>
   );
 }
