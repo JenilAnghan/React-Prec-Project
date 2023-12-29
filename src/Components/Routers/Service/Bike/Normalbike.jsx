d07d4cef669fbd7510c6e4158b53653de87baa77
jenil <jenilanghan508@gmail.com>
2023-12-29 13:04:24 +0530

    react

--- src/Components/Routers/Service/Bike/Normalbike.jsx
+++ src/Components/Routers/Service/Bike/Normalbike.jsx
@@ -1,5 +1,22 @@
+import { ArrowBigLeftDash } from "lucide-react";
 import React from "react";
+import { Navigate, useNavigate } from "react-router";
+import { Button } from "reactstrap";
 
 export default function Normalbike() {
-  return <h1>Normalbike</h1>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1>Normalbike</h1>
+      <Button
+        onClick={() => {
+          navigate(-1);
+        }}
+        className="p-3 justify-content-center align-item-center d-flex border-dark "
+        color="black"
+      >
+        <ArrowBigLeftDash />
+      </Button>
+    </>
+  );
 }
--- src/Components/Routers/Service/Bike/Sportsbike.jsx
+++ src/Components/Routers/Service/Bike/Sportsbike.jsx
@@ -1,12 +1,15 @@
 import { Button } from "reactstrap";
 import { ArrowBigLeftDash } from "lucide-react";
 import React from "react";
+import { useNavigate } from "react-router";
 
 export default function Sportsbike() {
+  const navigate = useNavigate();
   return (
     <>
       <h1>Sportsbike</h1>;
       <Button
+        onClick={() => navigate(-1)}
         className="p-3 justify-content-center align-item-center d-flex border-dark "
         color="black"
       >
