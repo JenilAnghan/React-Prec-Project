2fb10e3a11141dfec228bbaa60698a78e818ffdb
jenil <jenilanghan508@gmail.com>
2023-12-29 12:42:07 +0530

    react

--- src/Components/Routers/Service/Bike/Sportsbike.jsx
+++ src/Components/Routers/Service/Bike/Sportsbike.jsx
@@ -1,5 +1,17 @@
+import { Button } from "reactstrap";
+import { ArrowBigLeftDash } from "lucide-react";
 import React from "react";
 
 export default function Sportsbike() {
-  return <h1>Sportsbike</h1>;
+  return (
+    <>
+      <h1>Sportsbike</h1>;
+      <Button
+        className="p-3 justify-content-center align-item-center d-flex border-dark "
+        color="black"
+      >
+        <ArrowBigLeftDash />
+      </Button>
+    </>
+  );
 }
