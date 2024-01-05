e03c3b3008dcfe22c811443ed168679c53d60abf
jenil <jenilanghan508@gmail.com>
2024-01-05 12:47:16 +0530

    react

--- src/Components/ProtectedRouter/ui/Components/Model/RegisterModel.jsx
+++ src/Components/ProtectedRouter/ui/Components/Model/RegisterModel.jsx
@@ -12,71 +12,125 @@ import {
   Form,
 } from "reactstrap";
 import Select from "react-select";
+const userTypeOptions = [
+  { value: "user", label: "User" },
+  { value: "admin", label: "Admin" },
+  { value: "employee", label: "Employee" },
+];
 export default function RegisterModel({ modal, toggle }) {
+  let [user, setUser] = useState({
+    email: "",
+    password: "",
+    gender: "",
+    hobbes: [],
+    userType: "",
+  });
+
+  const checkBoxXHandler = (item) => {
+    const matchItem = check?.includes(item);
+    if (matchItem) {
+      let filter = check?.filter((e) => e !== item);
+      setCheck(filter);
+    } else {
+      setCheck([...check, item]);
+    }
+  };
   return (
-    <>
+    <div>
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>Modal title</ModalHeader>
         <ModalBody>
           <Form>
             <FormGroup>
-              <Label for="exampleEmail">Email</Label>
+              <Label for="Email">Email</Label>
               <Input
-                id="exampleEmail"
+                id="Email"
                 name="email"
-                placeholder="with a placeholder"
+                onChange={(e) => setUser({ ...user, email: e?.target?.value })}
+                placeholder="Enter Your Email"
                 type="email"
               />
             </FormGroup>
             <FormGroup>
-              <Label for="examplePassword">Password</Label>
+              <Label for="Password">Password</Label>
               <Input
-                id="examplePassword"
+                id="Password"
                 name="password"
-                placeholder="password placeholder"
+                onChange={(e) =>
+                  setUser({ ...user, password: e?.target?.value })
+                }
+                placeholder="Enter Your Password"
                 type="password"
               />
             </FormGroup>
-
             <Label>Gender</Label>
             <FormGroup tag="fieldset" className="d-flex gap-3">
               <FormGroup check>
-                <Input name="radio1" type="radio" />
+                <Input
+                  checked={user?.gender === "male"}
+                  onChange={() => setUser({ ...user, gender: "male" })}
+                  name="radio1"
+                  type="radio"
+                />
                 <Label check>Male </Label>
               </FormGroup>
               <FormGroup check>
-                <Input name="radio1" type="radio" />
+                <Input
+                  checked={user?.gender === "female"}
+                  onChange={() => setUser({ ...user, gender: "female" })}
+                  name="radio1"
+                  type="radio"
+                />
                 <Label check>Female</Label>
               </FormGroup>
               <FormGroup check disabled>
-                <Input name="radio1" type="radio" />
+                <Input
+                  checked={user?.gender === "kids"}
+                  onChange={() => setUser({ ...user, gender: "kids" })}
+                  name="radio1"
+                  type="radio"
+                />
                 <Label check>Kids</Label>
               </FormGroup>
             </FormGroup>
-
             <div>
               <Label>Hobbes</Label>
             </div>
-
             <FormGroup check inline>
-              <Input type="checkbox" />
+              <Input
+                checked={user?.hobbes?.includes("reading")}
+                t
+                type="checkbox"
+              />
               <Label check>Reading</Label>
             </FormGroup>
             <FormGroup check inline>
-              <Input type="checkbox" />
+              <Input
+                checked={user?.hobbes?.includes("traveling")}
+                t
+                type="checkbox"
+              />
               <Label check>Traveling</Label>
             </FormGroup>
-
             <FormGroup check inline>
-              <Input type="checkbox" />
+              <Input
+                checked={user?.hobbes?.includes("singing")}
+                t
+                type="checkbox"
+              />
               <Label check>singing</Label>
             </FormGroup>
             <FormGroup check inline>
-              <Input type="checkbox" />
+              <Input
+                checked={user?.hobbes?.includes("sports")}
+                type="checkbox"
+              />
               <Label check>Sports</Label>
             </FormGroup>
-
-            <Select />
+            <Select
+              onChange={(e) => setUser({ ...user, userType: e?.value })}
+              options={userTypeOptions}
+            />
 
             <Button color="danger" className="w-100 mt-3">
               Submit
@@ -84,6 +138,6 @@ export default function RegisterModel({ modal, toggle }) {
           </Form>
         </ModalBody>
       </Modal>
-    </>
+    </div>
   );
 }
--- src/Components/ProtectedRouter/ui/Pages/Home.jsx
+++ src/Components/ProtectedRouter/ui/Pages/Home.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import { Check } from "lucide-react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { useSearchParams } from "react-router-dom";
-import { Button } from "reactstrap";
+import { Button, Input } from "reactstrap";
 
 let nameArr = ["jenil", "nikunj", "jeel"];
 export default function Home() {
   const navigate = useNavigate();
 
+  let [check, setCheck] = useState(["male", "female"]);
+
+  const checkHandler = (item) => {
+    const matchItem = check?.includes(item);
+    if (matchItem) {
+      let filter = check?.filter((e) => e !== item);
+      setCheck(filter);
+    } else {
+      setCheck([...check, item]);
+    }
+  };
+
   return (
     <>
-    <h1>Home</h1>
-    <div className="ps-3">
-    <Button color="danger"   onClick={()=>navigate(-1)}>Go Back</Button>
-    </div>
+      <div className="ps-3 d-flex flex-column">
+        <h1>Home</h1>
+        {check.map((e, i) => {
+          return <h1 key={i}>{e}</h1>;
+        })}
+        <Input
+          checked={check?.includes("male")}
+          onClick={() => checkHandler("male")}
+          type="checkbox"
+        />
+        <label check>Male</label>
+        <Input
+          checked={check?.includes("female")}
+          onClick={() => checkHandler("female")}
+          type="checkbox"
+        />
+        <label check>FeMale</label>
+        <Input
+          checked={check?.includes("kids")}
+          onClick={() => checkHandler("kids")}
+          type="checkbox"
+        />
+        <label check>Kids</label>
+
+        <Button color="danger" onClick={() => navigate(-1)}>
+          Go Back
+        </Button>
+      </div>
     </>
   );
 }
